1b6a0fee33d9df491824587efc92963bae9d0078
Documenter.jl <documenter@juliadocs.github.io>
2021-11-02 19:08:21 +0000

    build based on 038f0fa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UnzipUser","category":"page"},{"location":"#UnzipUser","page":"Home","title":"UnzipUser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UnzipUser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UnzipUser]","category":"page"}]
+}
